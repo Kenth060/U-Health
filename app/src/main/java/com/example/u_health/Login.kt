6d9976a7da453db69026ad6c8f67f28eb51091b7
Kenth060 <133837320+Kenth060@users.noreply.github.com>
2023-05-31 18:24:01 -0600

    Third Commit

---
+++ app/src/main/java/com/example/u_health/Login.kt
@@ -0,0 +1,11 @@
+package com.example.u_health
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/example/u_health/SplashScreen.kt
+++ app/src/main/java/com/example/u_health/SplashScreen.kt
@@ -2,10 +2,43 @@ package com.example.u_health
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import com.example.u_health.databinding.ActivitySplashScreenBinding
 
-class SplashScreen : AppCompatActivity() {
+class SplashScreen : AppCompatActivity()
+{
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+
+        try
+        {
+            super.onCreate(savedInstanceState)
+            binding=ActivitySplashScreenBinding.inflate(layoutInflater)
+            val view=binding.root
+            setContentView(view)
+
+            val LogoApp: ImageView = binding.imgSplashScreen
+            val AnimLogo: Animation = AnimationUtils.loadAnimation(this,
+                R.anim.anim1)
+            LogoApp.startAnimation(AnimLogo)
+
+            AnimLogo.setAnimationListener(object: Animation.AnimationListener {
+
+                override fun onAnimationStart(animation: Animation?) {
+                }
+                override fun onAnimationEnd(animation: Animation?) {
+                }
+                override fun onAnimationRepeat(animation: Animation?) {
+                }
+            })
+        }
+        catch (e:java.lang.Exception)
+        { e.printStackTrace() }
+
+
     }
 }
