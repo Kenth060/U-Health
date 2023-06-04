623ebf40c47ed1d69141d4088e1a612b245c0c3a
Kenth060 <133837320+Kenth060@users.noreply.github.com>
2023-06-03 19:33:16 -0600

    9na Commit

--- app/src/main/java/com/example/u_health/Login.kt
+++ app/src/main/java/com/example/u_health/Login.kt
@@ -5,20 +5,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.u_health.databinding.ActivityLoginBinding
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FacebookAuthCredential
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class Login : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private val GOOGLE_SIGN_IN = 100
+    private val callbackManager = CallbackManager.Factory.create()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding=ActivityLoginBinding.inflate(layoutInflater)
-        val view=binding.root
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
         setContentView(view)
 
         val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
@@ -41,31 +55,87 @@ class Login : AppCompatActivity() {
             }
         }
 
-        binding.btnSignIn.setOnClickListener { v->
+        binding.btnSignIn.setOnClickListener { v ->
             val intent = Intent(v.context, CrearCuenta::class.java)
             startActivity(intent)
         }
+
+        binding.btnGoogleSignIn.setOnClickListener {
+            val Googleconf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+            val googleClient = GoogleSignIn.getClient(this, Googleconf)
+
+            googleClient.signOut()
+
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN)
+
+        }
+
+
+
+
     }
 
-    fun validarUsuario():Boolean{
+    fun validarUsuario(): Boolean {
         try {
-            var validaok:Boolean=false
-            if(binding.txtCorreo.text?.length?.equals(0)!!){
+            var validaok: Boolean = false
+            if (binding.txtCorreo.text?.length?.equals(0)!!) {
                 binding.txtCorreo.requestFocus()
                 binding.txtCorreo.setError("Debe ingresar su correo electronico.")
                 return validaok
             }
-            if(binding.txtContraseA.text?.length?.equals(0)!!){
+            if (binding.txtContraseA.text?.length?.equals(0)!!) {
                 binding.txtContraseA.requestFocus()
                 binding.txtContraseA.setError("Debe ingresar una contraseña.")
                 return validaok
             }
-            validaok=true
+            validaok = true
             return validaok
-        }catch (e:Exception){
-            e.message?.let{ Log.e("Error en validacion",it)};
+        } catch (e: Exception) {
+            e.message?.let { Log.e("Error en validacion", it) };
             return false;
         }
     }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GOOGLE_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                val account = task.getResult(ApiException::class.java)
+
+                if (account != null) {
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+
+                    FirebaseAuth.getInstance().signInWithCredential(credential)
+                        .addOnCompleteListener(this, OnCompleteListener<AuthResult> { task ->
+                            if (task.isSuccessful) {
+                                startActivity(Intent(this, MainActivity::class.java))
+                                Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show();
+                                binding.txtCorreo.setText("")
+                                binding.txtContraseA.setText("")
+                            }
+                        })
+                }
+            } catch (e: ApiException) {
+                val Mensaje = AlertDialog.Builder(this)
+                Mensaje.setTitle("Algo ha Fallado")
+                Mensaje.setMessage("No se ha podido agregar la cuenta correctamente, intente nuevamente")
+                Mensaje.setPositiveButton("Aceptar", null)
+                val dialog: AlertDialog = Mensaje.create()
+                dialog.show()
+            }
+        }
+    }
+
 
 }
