6f14e1356a9b6ea6fe65f91631e6da7baa52940d
Kenth060 <133837320+Kenth060@users.noreply.github.com>
2023-06-07 22:42:33 -0600

    13va Commit Primera Revision

--- app/src/main/java/com/example/u_health/MainActivity.kt
+++ app/src/main/java/com/example/u_health/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.u_health
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.u_health.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        reemplazo(Fragment_today())
+        binding.BottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.btnHoy -> reemplazo(Fragment_today())
+                R.id.btnProgreso -> reemplazo(Fragment_bars())
+                R.id.btnSoporte -> reemplazo(Fragment_soporte())
+                R.id.btnTerapia -> reemplazo(Fragment_terapia())
+                R.id.btnRecordatorios -> reemplazo(Fragment_recordatorios())
+                else -> {
+
+                }
+            }
+            true
+        }
+
+    }
+    private fun reemplazo(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaccion = fragmentManager.beginTransaction()
+        fragmentTransaccion.replace(R.id.fragment_layout,fragment)
+        fragmentTransaccion.commit()
     }
 }
