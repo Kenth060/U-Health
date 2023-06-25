2e8582fae1d8a3f930d3c99325f15fc539e3c7e2
Kenneth <kennethsilvah@gmail.com>
2023-06-25 15:37:19 -0600

    Segundo Cambio

--- app/src/main/java/com/example/u_health/fragmentos/Mapa.kt
+++ app/src/main/java/com/example/u_health/fragmentos/Mapa.kt
@@ -1,6 +1,7 @@
 package com.example.u_health.fragmentos
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Instrumentation.ActivityResult
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -38,6 +39,7 @@ class Mapa : Fragment() , OnMapReadyCallback {
     private var fbinding: FragmentMapaBinding? = null
     private val binding get() = fbinding!!
     private lateinit var MAP:GoogleMap
+    companion object{ const val REQUEST_CODE_LOCATION = 777}
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,7 +48,6 @@ class Mapa : Fragment() , OnMapReadyCallback {
         // Inflate the layout for this fragment
         fbinding = FragmentMapaBinding.inflate(inflater, container, false)
         val view: View = binding.root
-
 
         CreateMapFragment()
         return view
@@ -64,25 +65,6 @@ class Mapa : Fragment() , OnMapReadyCallback {
         MAP=googleMap
         MAP.setMapStyle(MapStyleOptions.loadRawResourceStyle(requireContext(),R.raw.mapa_style))
 
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            return
-        }
-        MAP.isMyLocationEnabled=true
-
-
-
-
-        val zoom = 16f
-        val miubicacion = LatLng(12.11170942302386, -86.23239262022197)
-        MAP?.animateCamera(CameraUpdateFactory.newLatLngZoom(miubicacion, zoom))
-
         MAP.setOnPoiClickListener {
 
             Toast.makeText(requireContext(), "Abriendo Navegacion Paso a Paso", Toast.LENGTH_SHORT).show()
@@ -96,12 +78,74 @@ class Mapa : Fragment() , OnMapReadyCallback {
         }
 
         val permiso=true
-
         MAP.uiSettings.isMapToolbarEnabled=permiso
         MAP.uiSettings.isCompassEnabled=permiso
         MAP.uiSettings.isScrollGesturesEnabled=permiso
         MAP.uiSettings.isZoomControlsEnabled=permiso
+
+       enableLocation()
+
+        val zoom = 16f
+        val miubicacion = LatLng(12.11170942302386, -86.23239262022197)
+        MAP?.animateCamera(CameraUpdateFactory.newLatLngZoom(miubicacion, zoom))
     }
 
+    private fun isLocationPermissionGranted()= ContextCompat.checkSelfPermission(requireContext(),
+        Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED
 
+
+    private fun enableLocation()
+    {
+        if(!::MAP.isInitialized) return
+        if(isLocationPermissionGranted())
+        {MAP.isMyLocationEnabled = true}
+        else
+        {requestLocationPermission()}
+    }
+
+    private fun requestLocationPermission()
+    {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this.requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION))
+        { Toast.makeText(requireContext(),"Acepte los Permisos de Ubicacion en Ajustes",
+            Toast.LENGTH_SHORT).show() }
+        else
+        {
+            ActivityCompat.requestPermissions(this.requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    )
+    {
+        when(requestCode)
+        {
+            REQUEST_CODE_LOCATION -> if(grantResults.isNotEmpty() && grantResults[0]
+                == PackageManager.PERMISSION_GRANTED)
+            { MAP.isMyLocationEnabled=true}
+            else
+            {Toast.makeText(requireContext(),"Para activar la localizacion ve a Ajustes y acepta los permisos",
+                Toast.LENGTH_SHORT).show()}
+
+
+            else ->{}
+        }
+    }
+
+    override fun onResume()
+    {
+        super.onResume()
+        if(!::MAP.isInitialized) return
+        if(!isLocationPermissionGranted())
+        {
+            MAP.isMyLocationEnabled=false
+            Toast.makeText(requireContext(),"Para activar la localizacion ve a Ajustes y acepta los permisos",
+                Toast.LENGTH_SHORT).show()
+        }
+
+    }
 }
