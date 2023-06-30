e360ee41a959c0c383022fa9bd58528fcf6708ba
jesus ampie <jesus.ampie.2021@gmail.com>
2023-06-30 17:19:57 -0600

    notificacion

--- app/src/main/java/com/example/u_health/fragmentos/FragmentCitas.kt
+++ app/src/main/java/com/example/u_health/fragmentos/FragmentCitas.kt
@@ -1,72 +1,135 @@
 package com.example.u_health.fragmentos
 
+import android.app.AlarmManager
+import android.app.AlertDialog
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
-import android.content.SharedPreferences
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.u_health.Adapters.AdapterCitas
-import com.example.u_health.Adapters.CitasListener
-import com.example.u_health.Adapters.MedicamentosProvider
-import com.example.u_health.R
+import androidx.annotation.RequiresApi
+import com.example.u_health.Notification
+import com.example.u_health.channelID
 import com.example.u_health.databinding.FragmentCitasBinding
-import com.example.u_health.model.Citas
+import com.example.u_health.messageExtra
+import com.example.u_health.notificationID
+import com.example.u_health.titleExtra
+import java.util.Calendar
+import java.util.Date
 
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
-class FragmentCitas : Fragment(),CitasListener
-{
-
+class FragmentCitas : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
     private var _binding: FragmentCitasBinding? = null
     private val binding get() = _binding!!
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentCitasBinding.inflate(inflater, container, false)
         val view = binding.root
+        //Jesus
 
-
-        binding.btnAddCita.setOnClickListener {
-
-            Navigation.findNavController(view).navigate(R.id.fragmentAddCita)
-
-            binding.btnAddCita.visibility = View.INVISIBLE
+        createNotificacionChannnel()
+        binding.btnSave.setOnClickListener {
+            scheduleNotification()
         }
 
-        initRecyclerView()
 
         return view
     }
 
-    private fun initRecyclerView()
-    {
-        val rv = binding.rvCitas
-        rv.layoutManager = LinearLayoutManager(requireContext())
-        rv.adapter= AdapterCitas(MedicamentosProvider.Recordatorios_Citas,this)
+    private fun scheduleNotification() {
+        val intent = Intent(requireContext(), Notification::class.java)
+        val title = "cita"
+        val message = "detalles"
+        intent.putExtra(titleExtra,title)
+        intent.putExtra(messageExtra,message)
+
+        val pendingIntent = PendingIntent.getBroadcast(
+            requireContext(),
+            notificationID,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val alarmManager = requireContext().getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val time = getTime()
+        alarmManager.setExactAndAllowWhileIdle(
+            AlarmManager.RTC_WAKEUP,
+            time,
+            pendingIntent
+        )
+        mensajeAlerta(time,title,message)
     }
 
-    override fun onCitaClicked(C: Citas)
-    {
-        view?.let { Navigation.findNavController(it).navigate(R.id.fragment_Recordatorio_Medico) }
-        val Preferencias: SharedPreferences? = context?.getSharedPreferences("Datos_Citas", Context.MODE_PRIVATE)
-        val editor = Preferencias?.edit()
-        editor?.putString("Titulo", C.Titulo)
-        editor?.putString("Medico",C.Medico)
-        editor?.putString("Fecha", C.Fecha)
-        editor?.putString("Hora", C.Hora)
-        editor?.putString("Detalles", C.Detalles)
-        editor?.apply()
+    private fun mensajeAlerta(time: Long, title: String, message: String) {
+        val date = Date(time)
+        val dateFormat = android.text.format.DateFormat.getLongDateFormat(requireContext())
+        val timeFormat = android.text.format.DateFormat.getTimeFormat(requireContext())
+
+        AlertDialog.Builder(requireContext())
+            .setTitle("Notificacion")
+            .setMessage(
+                "title" + title+
+                        "\nMessage: "+message+
+                        "\nAt: "+dateFormat.format(date) + " "+timeFormat.format(date))
+            .setPositiveButton("okay"){_,_ ->}
+            .setNegativeButton("Cancelar"){_,_ ->}
+            .show()
     }
 
+    private fun getTime(): Long {
+        val calendar = Calendar.getInstance()
+        val minutos = binding.timePicker.minute
+        val horas = binding.timePicker.hour
+        val day = binding.datePicker.dayOfMonth
+        val month = binding.datePicker.month
+        val year = binding.datePicker.year
 
+        calendar.set(year, month,day,horas,minutos)
+        return calendar.timeInMillis
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificacionChannnel() {
+        val name = "Notif channel"
+        val desc = "A Description of the Channel"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(channelID, name, importance)
+        channel.description = desc
+
+        val notificationManager = requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            FragmentCitas().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
 }
